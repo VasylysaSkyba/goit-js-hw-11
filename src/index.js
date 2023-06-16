b06705e39e12dad61a85bbf9af6f49fcfec293c3
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-06-16 21:02:21 +0300

    import

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
+import { PixabayApiService } from './js/pixabay-api';
---
+++ src/js/pixabay-api.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export class PixabayApiService {
+    #API_KEY = '37402833-717710cde1f8571f8393b1a39';
+    #BASE_URL = 'https://pixabay.com/api/'
+}
