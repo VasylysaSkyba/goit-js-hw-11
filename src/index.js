0d14ec15acc2077e3b16a5c521af61069851ef66
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-06-17 01:14:03 +0300

    css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './css/styles.css';
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів SimpleLightbox
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { PixabayApiService } from './js/pixabay-api';
 
 const refs = {
